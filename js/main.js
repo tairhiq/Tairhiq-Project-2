716cad42d8fe11a2c598ddd0c2b6f8cb0b49956f
tairhiq <tairhiqharris@yahoo.com>
2024-10-15 08:49:15 -0500

    images and css

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 // JS scripts placed here
+// JS scripts placed here
+const myButton = document.getElementById("intro");
+const myButton2 = document.getElementById("submit");
+console.log(myButton);
+console.log(myButton2);
+
+const element = document.getElementsByTagName("p");
+console. log(element);
+
+const y = document.querySelector(".logo")
+consol.log(y);
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+    }
+  }
